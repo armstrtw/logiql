a1ff3a31a83d1b5d3de2527b19c13166f89481d0
whit <armstrong.whit@gmail.com>
2015-05-04 10:52:03 -0400

    cleanup daycount policies and add fixed year basis daycounts Actual365Fixed, Actual364Fixed, Actual360Fixed

--- logiql/daycount.policies.hpp
+++ logiql/daycount.policies.hpp
@@ -7,50 +7,66 @@
 namespace logiql {
   using namespace boost::gregorian;
 
-  class ActualActual {
+  class DayCounter {
+  public:
+    static double julianDiff(date accrual_start,date accrual_end) {
+      return date_duration(accrual_end - accrual_start).days();
+    }
+
+    static int daysInYear(greg_year y) {
+      return gregorian_calendar::is_leap_year(y) ? 366 : 365;
+    }
+
+    static void checkAccrualDates(date accrual_start,date accrual_end) {
+      if(accrual_start > accrual_end) {
+        throw std::range_error("Accrual Start date > Accrual End Date.");
+      }
+    }
+  };
+
+  class ActualActual : DayCounter {
   public:
     static double yearFraction(date accrual_start, date accrual_end) {
+      checkAccrualDates(accrual_start,accrual_end);
+
+      if(accrual_end.year() - accrual_start.year() > 1) {
+        throw std::range_error("Need special Accrual method for coupons spanning more than 1 year.");
+      }
+
       // can use same year basis if:
       // start and end in same period
-      // both years are non leap years
+      // both years have same number of days
       // if dates cross a leap year / non leap year or non leap year/ leap year
-      // then we need two periods w/ different year basis
-      if(accrual_start.year() == accrual_end.year() ||
-         gregorian_calendar::is_leap_year(accrual_start.year()) == gregorian_calendar::is_leap_year(accrual_end.year())) {
-        double accural_days = date_duration(accrual_end - accrual_start).days();
-        double days_in_year = gregorian_calendar::is_leap_year(accrual_start.year()) ? 366 : 365;
+      // then we need two periods each with different year basis
+      if(accrual_start.year() == accrual_end.year() || daysInYear(accrual_start.year())==daysInYear(accrual_end.year())) {
+        double accural_days = julianDiff(accrual_start,accrual_end);
+        double days_in_year = daysInYear(accrual_start.year());
         return accural_days/days_in_year;
       } else {
-        // need two calculation periods
-        if(accrual_end.year() - accrual_start.year() > 1) {
-          throw std::range_error("Need special Accrual method for coupons spanning more than 1 year.");
-        }
-
         date p1_end(accrual_start.year(),12,31);
-        double accural_days_p1 = date_duration(p1_end - accrual_start).days();
-        double accural_days_p2 = date_duration(accrual_end - p1_end).days();
+        double accural_days_p1 = julianDiff(accrual_start,p1_end);
+        double accural_days_p2 = julianDiff(p1_end,accrual_end);
 
-        double days_in_year_p1 = gregorian_calendar::is_leap_year(accrual_start.year()) ? 366 : 365;
-        double days_in_year_p2 = gregorian_calendar::is_leap_year(accrual_end.year()) ? 366 : 365;
+        double days_in_year_p1 = daysInYear(accrual_start.year());
+        double days_in_year_p2 = daysInYear(accrual_end.year());
 
-        return accural_days_p1 / days_in_year_p1 + accural_days_p2/ days_in_year_p2;
+        return accural_days_p1 / days_in_year_p1 + accural_days_p2 / days_in_year_p2;
       }
     }
   };
 
-  /*
-  class Actual365 {
+
+  template<int N>
+  class ActualFixed : DayCounter {
   public:
-    double yearFraction(date accrual_start, date accrual_end) {
-      const double year_basis(365);
-      if(accrual_start.year() != accrual_end.year()) {
-        throw std::range_error("Accrual dates not in same year.");
-      }
-      
-      double accural_days = date_period(accrual_start,accrual_end).length();
-      return accural_days/year_basis;
+    static double yearFraction(date accrual_start, date accrual_end) {
+      checkAccrualDates(accrual_start,accrual_end);
+      return julianDiff(accrual_start,accrual_end)/static_cast<double>(N);
     }
   };
-  */
+
+  typedef ActualFixed<365> Actual365Fixed;
+  typedef ActualFixed<364> Actual364Fixed;
+  typedef ActualFixed<360> Actual360Fixed;
 
 } // namespace logiql
--- test/fixed.coupon.bond.test.cpp
+++ test/fixed.coupon.bond.test.cpp
@@ -4,10 +4,18 @@
 using namespace logiql;
 using namespace boost::gregorian;
 
+void printCashflows(Investment& b) {
+    CashFlowsT b_cfs = b.cashflows();
+    for ( auto cf : b_cfs) {
+      std::cout << cf.first << " " << cf.second << std::endl;
+    }
+}
+
 int main() {
-  FixedCouponBond<ActualActual,NoAdjustment,BackwardFromMaturity> b(date(2000,1,1),date(2015,12,31),
-                                                                    5.25,PaymentFrequencyT::SemiAnnual,100);
-  CashFlowsT b_cfs = b.cashflows();
-  for ( auto cf : b_cfs) std::cout << cf.first << " " << cf.second << std::endl;
+  FixedCouponBond<ActualActual,NoAdjustment,BackwardFromMaturity> actual_actual(date(2000,1,1),date(2015,12,31),5.25,PaymentFrequencyT::SemiAnnual,100);
+  printCashflows(actual_actual);
+
+  FixedCouponBond<Actual365Fixed,NoAdjustment,BackwardFromMaturity> actual_365(date(2000,1,1),date(2015,12,31),5.25,PaymentFrequencyT::SemiAnnual,100);
+  printCashflows(actual_365);
   return 0;
 }
