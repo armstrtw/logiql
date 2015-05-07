eed025131419658c76eced9c5879ed285ecc0770
whit <armstrong.whit@gmail.com>
2015-05-07 15:36:53 -0400

    add observed.partial.date and corresponding test

--- logiql/simple.types.hpp
+++ logiql/simple.types.hpp
@@ -3,6 +3,14 @@
 #include <utility>
 #include <vector>
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <logiql/observed.partial.date.hpp>
+
+// extend boost::gregorian to include observed_partial_date
+namespace boost {
+  namespace gregorian {
+      typedef date_time::observed_partial_date<boost::gregorian::date> observed_partial_date;
+  }
+}
 
 namespace logiql {
   typedef std::pair<boost::gregorian::date,double> CashFlowT;
