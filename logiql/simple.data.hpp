cfcd2cadbd3dddb236e579548b4031ea80e9041e
whit <armstrong.whit@gmail.com>
2017-02-15 10:45:01 -0500

    drop paymentFrequencyMonths

--- logiql/simple.data.hpp
+++ logiql/simple.data.hpp
@@ -4,14 +4,6 @@
 #include <logiql/simple.types.hpp>
 
 namespace logiql {
-  /*
-  const std::map<PaymentFrequencyT, int> paymentFrequencyMonths {
-    {PaymentFrequencyT::Annual, 12},
-    {PaymentFrequencyT::SemiAnnual, 6},
-    {PaymentFrequencyT::Quarterly, 3},
-    {PaymentFrequencyT::Monthly, 1}
-  };
-  */
 
   const std::map<PaymentFrequencyT, int> paymentFrequencyPaymentsPerYear {
     {PaymentFrequencyT::Annual, 1},
