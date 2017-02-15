13cf5c2d2f0caaf502f1ad041c7d6fe6aaa44c2c
whit <armstrong.whit@gmail.com>
2017-02-15 10:22:53 -0500

    drop paymentFrequencyToMonths and paymentFrequencyToPaymentsPerYear - use map instead

--- logiql/logiql.utils.hpp
+++ logiql/logiql.utils.hpp
@@ -10,32 +10,6 @@ namespace logiql {
       return date_duration(accrual_end - accrual_start).days();
   }
 
-  int paymentFrequencyToMonths(PaymentFrequencyT freq) {
-    switch(freq) {
-    case PaymentFrequencyT::Annual: return 12;
-    case PaymentFrequencyT::SemiAnnual: return 6;
-    case PaymentFrequencyT::Quarterly: return 3;
-    case PaymentFrequencyT::Monthly: return 1;
-    default:
-      throw std::range_error("Payment frequency" + std::to_string(static_cast<int>(freq)) + " not found");
-    }
-    // never gets here
-    return 0;
-  }
-
-  int paymentFrequencyToPaymentsPerYear(PaymentFrequencyT freq) {
-    switch(freq) {
-    case PaymentFrequencyT::Annual: return 1;
-    case PaymentFrequencyT::SemiAnnual: return 2;
-    case PaymentFrequencyT::Quarterly: return 4;
-    case PaymentFrequencyT::Monthly: return 12;
-    default:
-      throw std::range_error("Payment frequency" + std::to_string(static_cast<int>(freq)) + " not found");
-    }
-    // never gets here
-    return 0;
-  }
-
   template<typename T, typename U>
   T vectorShift(const T& vec, const U shift) {
     T ans(vec);
